73e23a23caa782ae74c8811dbbbe1fa99ccf79d3
timmythetrtl <89177115+timmythetrtl@users.noreply.github.com>
2023-12-06 22:40:58 -0600

    Cutscene Prototype

--- Assets/Scripts/TestScene/Test.cs
+++ Assets/Scripts/TestScene/Test.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Test : MonoBehaviour, IInteractable
 {
-
     public GameObject objectToActivate;
+    public PlayableDirector timeline;
 
     public void Interact()
     {
         // Check if the object to activate is not null
-            if (objectToActivate != null)
-            {
-                // Activate the specified object
-                objectToActivate.SetActive(true);
-            }
+        if (objectToActivate != null)
+        {
+            // Activate the specified object
+            objectToActivate.SetActive(true);
+        }
+
+        // Play the timeline
+        if (timeline != null)
+        {
+            timeline.Play();
+        }
     }
 }
