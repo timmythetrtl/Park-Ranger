2e4b244816bf84bd87890ca691158b573b207a0a
timmythetrtl <89177115+timmythetrtl@users.noreply.github.com>
2024-01-16 00:19:50 -0600

    Animation progress

--- Assets/Scripts/TestScene/Test.cs
+++ Assets/Scripts/TestScene/Test.cs
@@ -7,6 +7,17 @@ public class Test : MonoBehaviour, IInteractable
 {
     public GameObject objectToActivate;
     public PlayableDirector timeline;
+    public GameObject playerObject; // Reference to the player object you want to disable
+
+    private void Start()
+    {
+        // Subscribe to the timeline events
+        if (timeline != null)
+        {
+            timeline.played += OnTimelinePlayed;
+            timeline.stopped += OnTimelineStopped;
+        }
+    }
 
     public void Interact()
     {
@@ -17,10 +28,32 @@ public class Test : MonoBehaviour, IInteractable
             objectToActivate.SetActive(true);
         }
 
+        // Disable the player object if it's not null
+        if (playerObject != null)
+        {
+            playerObject.SetActive(false);
+        }
+
         // Play the timeline
         if (timeline != null)
         {
             timeline.Play();
         }
+    }
+
+    private void OnTimelinePlayed(PlayableDirector director)
+    {
+        // Debug.Log("Timeline started playing");
+    }
+
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+        // Reactivate the player object when the timeline is done playing
+        if (playerObject != null)
+        {
+            playerObject.SetActive(true);
+        }
+
+        // Debug.Log("Timeline stopped playing");
     }
 }
