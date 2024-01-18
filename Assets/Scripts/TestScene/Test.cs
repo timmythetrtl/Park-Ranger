1671456ac0ac854832e2ad56a5b46145732a40da
timmythetrtl <89177115+timmythetrtl@users.noreply.github.com>
2024-01-18 00:57:48 -0600

    diss record

--- Assets/Scripts/TestScene/Test.cs
+++ Assets/Scripts/TestScene/Test.cs
@@ -3,57 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class Test : MonoBehaviour, IInteractable
+public class Test : MonoBehaviour
 {
-    public GameObject objectToActivate;
     public PlayableDirector timeline;
-    public GameObject playerObject; // Reference to the player object you want to disable
 
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        // Subscribe to the timeline events
-        if (timeline != null)
+        if (other.CompareTag("Player")) // Check if the collider belongs to the player
         {
-            timeline.played += OnTimelinePlayed;
-            timeline.stopped += OnTimelineStopped;
+            // Play the timeline
+            if (timeline != null)
+            {
+                timeline.Play();
+                // Destroy the object after the timeline is played
+                Destroy(gameObject);
+            }
         }
-    }
-
-    public void Interact()
-    {
-        // Check if the object to activate is not null
-        if (objectToActivate != null)
-        {
-            // Activate the specified object
-            objectToActivate.SetActive(true);
-        }
-
-        // Disable the player object if it's not null
-        if (playerObject != null)
-        {
-            playerObject.SetActive(false);
-        }
-
-        // Play the timeline
-        if (timeline != null)
-        {
-            timeline.Play();
-        }
-    }
-
-    private void OnTimelinePlayed(PlayableDirector director)
-    {
-        // Debug.Log("Timeline started playing");
-    }
-
-    private void OnTimelineStopped(PlayableDirector director)
-    {
-        // Reactivate the player object when the timeline is done playing
-        if (playerObject != null)
-        {
-            playerObject.SetActive(true);
-        }
-
-        // Debug.Log("Timeline stopped playing");
     }
 }
